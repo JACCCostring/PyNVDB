10a8670b5091f8f532d8664f4298823531fc1942
JACCCostring <costring20@gmail.com>
2025-02-14 23:38:53 +0100

    UPDATE: adding new imports for strategies

--- src/__init__.py
+++ src/__init__.py
@@ -2,3 +2,5 @@ from .utils_class import UtilEnviroment, ConfigManager
 from .datacatalog_class import Datacatalog
 from .area_class import AreaLocation
 from .strategies import EgenskapStrategy
+from .strategies import KommuneStrategy
+from .strategies import FylkeStrategy
