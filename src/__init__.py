dd79ecf910a13e864371848430c9c212b5688128
JACCCostring <costring20@gmail.com>
2025-02-17 23:57:27 +0100

    CHANGES: organizing imports

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,9 @@
+from .uri_generator import EgenskapUriGenerator, KommuneUriGenerator, FylkeUriGenerator, VegrefUriGenerator
 from .utils_class import UtilEnviroment, ConfigManager
+from .consult_manager import ConsultManager
 from .datacatalog_class import Datacatalog
-from .area_class import AreaLocation
 from .strategies import EgenskapStrategy
 from .strategies import KommuneStrategy
-from .strategies import FylkeStrategy
 from .strategies import VegrefStrategy
-from .consult_manager import ConsultManager
-from .uri_generator import EgenskapUriGenerator, KommuneUriGenerator, FylkeUriGenerator, VegrefUriGenerator
+from .strategies import FylkeStrategy
+from .area_class import AreaLocation
