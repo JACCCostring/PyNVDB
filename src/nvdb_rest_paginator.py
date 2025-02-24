7570770696a59598664aef1d1707699e8b88e27a
JACCCostring <costring20@gmail.com>
2025-02-24 01:28:16 +0100

    CHANGES: consult manager fetching async better now

--- src/nvdb_rest_paginator.py
+++ src/nvdb_rest_paginator.py
@@ -16,111 +16,103 @@ class NVDB_REST_Paginator:
     def __init__(self, start_endpoint: str) -> None:
 
         self.___start_endpoint: str = start_endpoint
-        self.___page_amount: int = int()
-        self.___current_amount: int = int()
+        self.___page_amount: int = int() #will be discovered later after first fetch
+        self.___current_amount: int = int() #start amount for pagination after first fetch
         self.___records: list = []
-        self.___hrefs: set = set()
 
         if self.___start_endpoint != '':
             self.set_endpoint( self.___start_endpoint )
 
     def set_endpoint(self, endpoint: str) -> None:
 
-        if self.___start_endpoint != '':
+        if self.___start_endpoint == '':
             self.___start_endpoint = endpoint
         
         #start pagination, to gather all data need it
         asyncio.run( self.___paginate() )
     
-    def ___populate_href(self, data: list) -> None:
+    def ___populate_nvdb_data(self, data: dict) -> None:
 
         for item in data['objekter']:
 
-            self.___hrefs.add( item['href'] )
-    
-    def ___populate_nvdb_data(self, data: dict) -> None:
-
-        object = RoadObject( nvdbid=data['id'], version=data['metadata']['versjon'], 
-                            last_modified=data['metadata']['sist_modifisert'],
-                            geometry=data['geometri']['wkt']  )
+            object = RoadObject( nvdbid=item['id'], version=item['metadata']['versjon'], 
+                                last_modified=item['metadata']['sist_modifisert'],
+                                geometry=item['geometri']['wkt']  )
             
-        self.___records.append( object )
+            self.___records.append( object )
+
+    async def ___getandparse_nvdb_data(self, endpoint, session) -> None:
+
+        async with session.get( endpoint, headers=ConfigManager().load_config().get('test_headers') ) as request:
+            if request.ok:
+                return await request.json()
 
     async def ___paginate(self) -> None:
-        #getting first endpoint to get first pagination
-        endpoint = self.___start_endpoint
-        #until now, we dont know so 1 is ok it changes after frist pagination fetch
-        async with aiohttp.ClientSession() as session:
-
-            first_fetched = await self.___get_pagination_data(session, endpoint)
-
-            first_pagination = first_fetched['metadata']
-
-            self.___page_amount = first_pagination['antall']
-            self.___current_amount = first_pagination['returnert']
-
-            #getting next endpoint, after being done with first
-            endpoint = self.___start_endpoint + '&start=' + first_pagination['neste']['start']
-
-            #add start of href
-            self.___populate_href( first_fetched )
-
-        async with aiohttp.ClientSession() as session:
-
-            for _ in range(int(self.___page_amount / self.___current_amount)):
-                
-                result = await self.___get_pagination_data(session, endpoint)
-
-                pagination = result['metadata']
-
-                endpoint = pagination['neste']['href']
-
-                self.___current_amount += pagination['returnert']
-                    
-                print( self.___current_amount, ' of ', self.___page_amount)
-
-                self.___populate_href( result )
-        
-        '''
-            now that we have a list of href of paginated road objects
-            then we preceed to fetch all the road objects corresponding
-            to those href, only if href list is populated
-        '''
-        if len( self.___hrefs ) > 0:
-            print('preparing nvdb data')
-            await self.___prepare_nvdb_data()
-        
-    async def ___get_pagination_data(self, session, endpoint) -> dict:
-
-        response = await session.get(endpoint, headers=ConfigManager().load_config().get('test_headers'))
-
-        if response.ok:
-            return await response.json()
-    
-    async def ___prepare_nvdb_data(self) -> None:
         tasks: list = []
 
+        first_pag = await self.___first_pagination()
+
+        self.___page_amount = first_pag['antall']
+        # self.___current_amount = 1000 if first_pag['returnert'] <= 1000 else first_pag['returnert']
+        self.___current_amount = first_pag['returnert']
+
+        next_endpoint = first_pag['neste']['href']
+
+        '''
+            some times return data amount and total data amaount
+            if divide it is equal to 1 or 2 then we need to trick to get
+            that amount + 1, because of the loop, so we can get enough iterations
+            to get all task
+        '''
+        help_amount = self.___current_amount - 1 if int( self.___page_amount / self.___current_amount ) <= 2 else 0
+
+        print(help_amount)
+
+        print()
+        print('starting ...')
+        print('fetching', self.___page_amount, 'vegobjekter')
+
         async with aiohttp.ClientSession() as session:
-            for href in self.___hrefs:
-
-                task = asyncio.create_task( session.get( href, headers=ConfigManager().load_config().get('test_headers') ) )
-                tasks.append( task )
-            
-            print('done preparing!')
-
-            print('start executing task ...')
-
-            responses_coro = await asyncio.gather( *tasks )
-            
-            for response in responses_coro:
+            for _ in range( int( self.___page_amount / self.___current_amount ) + help_amount ):
                 
-                if response.ok:
-        
-                    self.___populate_nvdb_data( await response.json() )
-            
-            print( 'size href pagination:', len(self.___hrefs) )
-            print( 'size nvdb records:', len(self.___records) )
+                # print(next_endpoint)
+                print(self.___current_amount, ' of ', self.___page_amount)
 
+                task = asyncio.create_task( self.___getandparse_nvdb_data(next_endpoint, session) )
+                tasks.append( task )
+
+                next_pag = await self.___next_pagination(next_endpoint, session)
+                next_endpoint = next_pag['neste']['href']
+
+                # self.___current_amount += 1000 if first_pag['returnert'] <= 1000 else first_pag['returnert']
+                self.___current_amount += first_pag['returnert']
+
+                
+            print('ending fetch ...')
+            responses_coro = await asyncio.gather( *tasks )
+
+            print('processing ...')
+            for response in responses_coro:
+                self.___populate_nvdb_data( response )
+
+        for item in self.___records:
+            if item.nvdbid == 460057601:
+                print('id:', item.nvdbid, ' wkt:', item.geometry)
+
+    async def ___first_pagination(self) -> None:
+        async with aiohttp.ClientSession() as session:
+            response = await session.get( self.___start_endpoint, headers=ConfigManager().load_config().get('test_headers') )
+
+            if response.ok:
+                data = await response.json()
+                return data['metadata']
     
+    async def ___next_pagination(self, endpoint, session) -> None:
+        async with session.get( endpoint, headers=ConfigManager().load_config().get('test_headers') ) as response:
+
+            if response.ok:
+                data = await response.json()
+                return data['metadata']
+        
     def nvdb_data(self) -> list:
         return self.___records
