047fe617415d74e27e0219ea0e67ff64438275a5
JACCCostring <costring20@gmail.com>
2025-02-26 15:21:33 +0100

    CHANGES: strategy builder to improve when adding future strategies

---
+++ src/strategy_builder.py
@@ -0,0 +1,50 @@
+from .strategies import Strategy
+from .uri_generator import UriGenerator, EgenskapUriGenerator, FylkeUriGenerator, KommuneUriGenerator, VegrefUriGenerator
+from typing import List
+
+class StrategyBuilder:
+    def __init__(self) -> None:
+        self.___strategies: List[Strategy] = []
+        self.___strategies_promotion: List[dict[str, UriGenerator]] = []
+
+        #defining default promotions
+        default_promotions: dict[str, UriGenerator] = {
+            'egenskap': EgenskapUriGenerator(),
+            'fylke': FylkeUriGenerator(),
+            'kommune': KommuneUriGenerator(),
+            'vegsystemreferanse': VegrefUriGenerator()
+        }
+        
+        #adding default promotions
+        self.___strategies_promotion.append( default_promotions )
+    
+    def promote_strategy(self, new_promotion: str) -> None:
+        if new_promotion in self.___strategies_promotion:
+            raise Exception('Error: prmotion already exist!')
+        
+        # if new_promotion not in self.___strategies_promotion:
+        self.___strategies_promotion.append( new_promotion )
+
+    def add_strategy(self, strategy: Strategy) -> None:
+
+        if not isinstance(strategy, Strategy):
+            raise Exception('error: wrong type, strategy type required!')
+        
+        self.___strategies.append( strategy )
+
+    def get_strategy_uri(self, target_strategy: Strategy) -> str:
+
+        if not isinstance(target_strategy, Strategy):
+            raise Exception('error: wrong type, strategy type required!')
+        
+        #only if there is one or more strategies
+        if len( self.___strategies ) > 0:
+
+            for strategy in self.___strategies:
+                if strategy.strategy_type == target_strategy.strategy_type:
+                    #make generator instance
+                    for promotion in self.___strategies_promotion:
+                        uri_generator = promotion.get( strategy.strategy_type )
+                        return uri_generator.generate_uri( strategy )
+        
+        raise Exception('Error: not any strategy added!')
